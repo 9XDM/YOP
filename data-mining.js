2a2c9fedf0727660f7620ba3285b29b62737feec
myoungho.Pak <qkraudghgh@naver.com>
2017-01-14 21:53:38 +0900

    Add Parse script

---
+++ data-mining.js
@@ -0,0 +1,28 @@
+'use strict';
+const firebase = require('firebase');
+const _ = require('lodash');
+const fs = require('fs');
+
+const config = {
+  apiKey: "HI",
+  authDomain: "HI",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
+
+firebase.initializeApp(config);
+
+const ref = firebase.database().ref('/posts/');
+
+ref.once('value').then(snapshot => {
+  let bodyArray = [];
+  _.forEach(snapshot.val(), data => {
+    bodyArray.push(data.body);
+  });
+  bodyArray = _.join(bodyArray, ' ');
+  fs.writeFile('./results.txt', bodyArray, function(err) {
+    if(err) throw err;
+    console.log('File write completed');
+  });
+})
