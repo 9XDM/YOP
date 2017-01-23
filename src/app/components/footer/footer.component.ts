ca8a6044c57dc01316d7595825fe9ffb96eada7e
jayjin <jayjinjay@gmail.com>
2017-01-23 22:22:42 +0900

    Add '_blank' to footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -4,12 +4,12 @@ import {Component} from "@angular/core";
   template: `
     <footer class="page-footer">
       <div class="container">
-          <div class="">Made with ♥️️ <a href="https://github.com/qkraudghgh">@qkraudghgh</a>, <a href="https://github.com/milooy">@milooy</a>, <a href="https://github.com/Leop0ld">@Leop0ld</a>, <a href="https://github.com/kyeongwan">@kyeongwan</a>, <a href="https://github.com/kangsLee">@river-mountain</a> </div>
+          <div class="">Made with ♥️️ <a href="https://github.com/qkraudghgh" target="_blank">@qkraudghgh</a>, <a href="https://github.com/milooy" target="_blank">@milooy</a>, <a href="https://github.com/Leop0ld" target="_blank">@Leop0ld</a>, <a href="https://github.com/kyeongwan" target="_blank">@kyeongwan</a>, <a href="https://github.com/kangsLee" target="_blank">@river-mountain</a> </div>
       </div>
       <div class="footer-copyright">
           <div class="container">
-              <a href="https://github.com/9XDM/YOP" target="_blank">View Github Source</a>
-              <div class="right">© 2017 <a href="">9XD</a></div>
+              <a href="https://github.com/9XDM/YOP" target="_blank">View Github Source <span class="icon-github-alt"></span></a>
+              <div class="right">© 2017 <a href="https://www.facebook.com/groups/1565641083693087/" target="_blank">9XD</a></div>
           </div>
       </div>
   </footer>
